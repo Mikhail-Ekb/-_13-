3805c37b644a9f415bf75cdf3bc0743acc93d045
MIKHAIL <MIKHAIL@DESKTOP-6MU8OHN>
2022-11-27 22:10:10 +0500

    Добавьте файлы проекта.

---
+++ Задача_13_1_Работа над ошибками/Building1.cs
@@ -0,0 +1,24 @@
+﻿namespace Задача_13_1_Работа_над_ошибками
+{
+    internal class Building
+    {
+        private string Adress { get; set; }
+        private float Length { get; set; }
+        private float Width { get; set; }
+        private float Height { get; set; }
+
+
+        public Building(string adress, float length, float width, float height)
+        {
+            Adress = adress;
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public string Print()
+        {
+            return $"{Adress}\n{Length}\n{Width}\n{Height}";
+        }
+    }
+}
---
+++ Задача_13_1_Работа над ошибками/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_13_1_Работа_над_ошибками
+{
+    sealed class Multibuilding : Building
+    {
+        private int levels;
+
+        private int Levels
+        {
+            get
+            {
+                return levels;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    levels = 1;
+                }
+                else
+                {
+                    levels = value;
+                }
+            }
+        }
+
+        public Multibuilding(string adress, float length, float width, float heigt, int levels)
+            : base(adress, length, width, heigt)
+        {
+            Levels = levels;
+        }
+
+
+        public string Print()
+        {
+            string result = base.Print();
+            result += $"\n{levels}";
+            return result;
+        }
+    }
+}
